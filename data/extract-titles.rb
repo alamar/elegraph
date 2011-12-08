d85626948f57842aba4a9bb7330fd05133f18ec8
Илья Казначеев <ilya.kasnacheev@gmail.com>
2011-12-09 01:50:30 +0400

    A script to extract titles from page dumps.

---
+++ data/extract-titles.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'mechanize'
+
+client = WWW::Mechanize.new
+ARGV.each do |name|
+    titles = []
+    file = File.open name, "rb"
+    html = client.html_parser.parse file.read, nil, "WINDOWS-1251"
+    title_rows = html.xpath "/html/body/table[last()]/tr[last()]/td/table[last()]/tr/td[1]/table/tr[count(td) = 3]"
+    title_rows.each do |row|
+        titles.push row.xpath("td[2]").text.strip
+    end
+    file.close
+    out = File.open name + "titles", "wb"
+    out.puts titles.to_s
+    out.close
+end
+
+
+
