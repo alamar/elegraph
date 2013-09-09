584a96a6ec4883e1526b6264535741c9e1661165
Ilya Kasnacheev <ilyak@yandex-team.ru>
2013-09-09 23:03:39 +0400

    New window function and stuff.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp org.clojure-clojure-jar-1.5.0-RC1.jar:. clojure.main moscow.clj
+java -cp org.clojure-clojure-jar-1.5.0-RC1.jar:org.clojure-clojure-contrib-jar-1.2.0.jar:. clojure.main $@
