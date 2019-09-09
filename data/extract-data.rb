aaf8a8e4b675918be43c179b007f7890e34cd9fc
Ilya Kasnacheev <ilya.kasnacheev@gmail.com>
2019-09-09 23:04:02 +0300

    Moscow Electoral District 30 'Junemann' elections.

--- data/extract-data.rb
+++ data/extract-data.rb
@@ -6,7 +6,7 @@ ARGV.each do |name|
     STDERR.puts name
     file = File.open name, "rb"
     html = client.html_parser.parse file.read, nil, "WINDOWS-1251"
-    data_rows = html.xpath "/html/body/table[last()]/tr[last()]/td/table[last()]/tr/td[2]/div/table/tr[not(td[2]/@colspan)]"
+    data_rows = html.xpath "/html/body/table[last()]/tr[last()]/td/div/table/tr/td[2]/div/table/tr[position() > 1]"
     columns = data_rows[0].xpath("count(td)") + 1
     (1..columns).each do |i|
        numbers = []
